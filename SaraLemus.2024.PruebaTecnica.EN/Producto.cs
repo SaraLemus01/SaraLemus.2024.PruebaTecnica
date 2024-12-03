31fe7195a0c6451019dfdade07c469648e34dab0
SaraLemus01 <sara.lemus.2004.10@gmail.com>
2024-12-02 21:39:51 -0600

    creacion de vistas de categorias

--- SaraLemus.2024.PruebaTecnica.EN/Producto.cs
+++ SaraLemus.2024.PruebaTecnica.EN/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,15 @@ namespace SaraLemus._2024.PruebaTecnica.EN
     public class Producto
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Nombre no puede tener mas de 50 caracteres")]
         public string Nombre { get; set; }
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo Precio es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Precio no puede tener mas de 50 caracteres")]
         public decimal Precio { get; set; }
+        
         public int IdCategoria { get; set; }
     }
 }
